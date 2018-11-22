b67de75f1e04b7ff2a1828620e87ea62d2136146
Karthik T S <shivakarthik8@gmail.com>
2018-11-22 17:15:42 +0530

    added support pause-resume

--- work_analyzer.py
+++ work_analyzer.py
@@ -1,11 +1,29 @@
 import os,sys,time
 import codecs
+import threading
+import getpass
+
+session_over = True
+paused = False
+
+def handle_pause_resume():
+    global session_over
+    global paused
+    while(not session_over):
+        time.sleep(1)
+        interrupt = getpass.getpass(prompt="") 
+        if interrupt == "p":
+            paused = True
+        elif interrupt == "r":
+            paused = False
 
 def get_pretty_time(time_):
     local_time = time.asctime(time.localtime(time_))
     return local_time 
 
 def start_work_analyzer():
+    global session_over
+    global paused
 
     #print login time
     login_time = time.time()
@@ -13,20 +31,29 @@ def start_work_analyzer():
 
     num_sessions = 0
     session_duration_in_mins = 1
+    pauseresume_actor = None
 
     while True:
-        continue_work = input("start a {} min session [type yes to start, no to end work] :".format(session_duration_in_mins))
-        continue_work = continue_work == "yes"
+        if pauseresume_actor:
+            pauseresume_actor.join()
 
-        if continue_work:
+        continue_work = input("start a {} min session [type yes to start, no to end work] :".format(session_duration_in_mins))
+
+        if continue_work == "yes":
             #print session start time
             session_start_time = time.time()
             print("session start time: {}".format(get_pretty_time(session_start_time)))
             
-            #start the timer
             session_duration_in_mins_ = session_duration_in_mins
             session_duration_in_secs = session_duration_in_mins_*60
             timer_value = ""
+            
+            #start pauseresume handler thread
+            session_over = False
+            pauseresume_actor = threading.Thread(target=handle_pause_resume)
+            pauseresume_actor.start()
+            
+            #start the timer
             while session_duration_in_secs>=0:
                 for i in range(len(timer_value)):
                     sys.stdout.write('\b')
@@ -39,17 +66,24 @@ def start_work_analyzer():
                     session_duration_in_mins_ -= 1
                 session_duration_in_secs -= 1
 
+                while(paused):
+                    time.sleep(1)
+
             #print session end time
+            session_over = True
             session_end_time = time.time()
             print("\nsession end time: {}".format(get_pretty_time(session_end_time)))
 
             num_sessions += 1
 
-        else:
+        elif continue_work == "no":
             #print logoff time
             logoff_time = time.time()
             print("logoff time: {}".format(get_pretty_time(logoff_time)))
             break
+
+        else:
+            print("invalid input, please type in yes or no")
     
     #print today's efficiency    
     total_time_in_office = (logoff_time - login_time)/60
@@ -59,7 +93,7 @@ def start_work_analyzer():
     print("today's work efficiency percentage: {}".format(efficiency))
 
     with codecs.open("work_analyzer.db","a","utf-8") as db:
-        unit = "{}:{}".format(login_time,efficiency)
+        unit = "{}:{}\n".format(login_time,efficiency)
         db.write(unit)
 
     return
